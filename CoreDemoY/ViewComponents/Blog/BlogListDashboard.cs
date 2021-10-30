9609558313408daae4639c2e4bb02c55ba991219
ZahirZairov <zairli.zahir@bk.ru>
2021-10-31 00:42:50 +0400

    Asp.Net Core Blog Updated

---
+++ CoreDemoY/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemoY.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListWithCategory().OrderByDescending(x=>x.BlogId).Take(10).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ CoreDemoY/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemoY.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
